351c60b6bc3bb493da33fadc531c0647d59bd642
Manish <van.manish@gmail.com>
2019-12-10 16:26:13 +0530

    added Todo and retrieve data from service

--- src/main/java/com/in28min/rest/webservices/restfulwebservices/helloworld/TodoResource.java
+++ src/main/java/com/in28min/rest/webservices/restfulwebservices/helloworld/TodoResource.java
@@ -2,12 +2,14 @@ package com.in28min.rest.webservices.restfulwebservices.helloworld;
 
 import com.in28min.rest.webservices.restfulwebservices.helloworld.todo.ToDoHardCodedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoResource {
     @Autowired
